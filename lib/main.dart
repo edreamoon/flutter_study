902961f5f32dc82b244175771c5cd009c077b2cc
jianfeng.li <jianfeng.li@moji.com>
2019-01-18 18:28:39 +0800

    login page

--- lib/main.dart
+++ lib/main.dart
@@ -1,109 +1,140 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:flutter/rendering.dart';
+void main() => runApp(LoginAlertDemoApp());
 
-void main() => runApp(new MyApp());
-
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class LoginAlertDemoApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-      title: 'Flutter Demo',
-      theme: new ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or press Run > Flutter Hot Reload in IntelliJ). Notice that the
-        // counter didn't reset back to zero; the application is not restarted.
-        primarySwatch: Colors.blue,
+    return MaterialApp(
+      title: 'Login Alert',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Login Ale'),
+        ),
+        body: new LoginHomePage(),
       ),
-      home: new MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class LoginHomePage extends StatefulWidget {
   @override
-  _MyHomePageState createState() => new _MyHomePageState();
+  State<StatefulWidget> createState() {
+    return _LoginHomePageState();
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _LoginHomePageState extends State<LoginHomePage> {
+  final _formKey = GlobalKey<FormState>();
+  final _userNameTextController = TextEditingController();
+  final _passwordTextController = TextEditingController();
+  bool _showLoading = false;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  Future _loginRequest() async {
+    return Future.delayed(Duration(seconds: 3), () {
+      print('login success');
     });
+  }
+
+  void _toggleSubmit() {
+    if (_formKey.currentState.validate()) {
+      setState(() {
+        _showLoading = true;
+      });
+
+      _loginRequest().then((onValue) {
+        setState(() {
+          _showLoading = false;
+        });
+
+        showDialog(
+            context: context,
+            builder: (context) {
+              String alertText = 'login success!' +
+                  ' \nuserName:' +
+                  _userNameTextController.text +
+                  '\npassWord:' +
+                  _passwordTextController.text;
+
+              return AlertDialog(content: Text(alertText));
+            });
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return new Scaffold(
-      appBar: new AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: new Text(widget.title),
-      ),
-      body: new Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: new Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug paint" (press "p" in the console where you ran
-          // "flutter run", or select "Toggle Debug Paint" from the Flutter tool
-          // window in IntelliJ) to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
+    List<Widget> childrens = [];
+
+    final _mainConatiner = Container(
+      padding: EdgeInsets.all(10.0),
+      child: Form(
+        key: _formKey,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
-            new Text(
-              'You have pushed the button this many times:',
+            Image.asset(
+              'images/lake.jpg',
+              height: 180.0,
+              fit: BoxFit.fitHeight,
             ),
-            new Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
+            Padding(padding: EdgeInsets.only(top: 50.0)),
+            TextFormField(
+              decoration: InputDecoration(hintText: 'user name'),
+              validator: (value) {
+                if (value.isEmpty) return 'pls enter user name';
+              },
+              controller: _userNameTextController,
             ),
+            TextFormField(
+              decoration: InputDecoration(hintText: 'passowrd'),
+              validator: (value) {
+                if (value.isEmpty) return 'pls enter pwd';
+              },
+              controller: _passwordTextController,
+              obscureText: true,
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 20.0),
+            ),
+            SizedBox(
+              width: double.infinity,
+              height: 50,
+              child: RaisedButton(
+                onPressed: _toggleSubmit,
+                child: Text(
+                  'Submit',
+                  style: TextStyle(color: Colors.white, fontSize: 20),
+                ),
+                color: Colors.blue,
+              ),
+            )
           ],
         ),
       ),
-      floatingActionButton: new FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: new Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+
+    childrens.add(_mainConatiner);
+
+    final _loadingContainer = Container(
+        constraints: BoxConstraints.expand(),
+        color: Colors.black12,
+        child: Center(
+          child: Opacity(
+            opacity: 0.9,
+            child: SpinKitWave(
+              color: Colors.red,
+              size: 50.0,
+            ),
+          ),
+        ));
+    if (_showLoading) {
+      childrens.add(_loadingContainer);
+    }
+    return Stack(
+      children: childrens,
     );
   }
 }
